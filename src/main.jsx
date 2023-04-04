0380a07f7ceddde340fda7d11d8e4b4326f39486
iamnaymur <nrtusher1@gmail.com>
2023-04-04 13:07:26 +0600

    completed

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Root, {
   action as rootAction,
 } from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact, { loader as contactLoader } from "./routes/contact";
+import Contact, { loader as contactLoader, action as contactAction } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
 import Index from "./routes";
@@ -21,25 +21,31 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
-        index: true,
-        // path:'/', //? the path here works the same as the index. 
-        element: <Index></Index>,
-      },
-      {
-        path: "contacts/:contactId",
-        element: <Contact></Contact>,
-        loader: contactLoader,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact></EditContact>,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
-        errorElement: <div>opps! There was an error.</div>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+          {
+            index: true,
+            // path:'/', //? the path here works the same as the index.
+            element: <Index></Index>,
+          },
+          {
+            path: "contacts/:contactId",
+            element: <Contact></Contact>,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact></EditContact>,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>opps! There was an error.</div>,
+          },
+        ],
       },
     ],
   },
